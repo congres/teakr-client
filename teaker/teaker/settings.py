221ee123883d027cdbdea2902fb30ab5200643ba
Jules Kzl <jules.kozolinsky@ens-cachan.fr>
2018-07-03 22:14:38 +0200

    authorized hosts

--- teaker/teaker/settings.py
+++ teaker/teaker/settings.py
@@ -26,7 +26,7 @@ with open('../secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://0.0.0.0:8000/']
 
 
 # Application definition
